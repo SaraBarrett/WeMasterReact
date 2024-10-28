49237a5283538cc337d49151019c37fc90aa63eb
SaraBarrett <sara.c.mon@gmail.com>
2024-10-28 10:44:28 +0000

    exercises

---
+++ src/exs/components/delete.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+    return (
+      <div>
+        <div data-testid="alert" id="alert">
+          <h2>Are you sure?</h2>
+          <p>These changes can't be reverted!</p>
+          <button>Proceed</button>
+        </div>
+        <button>Delete</button>
+      </div>    
+    );
+}
