021ed173d0cb33d2332b7fec7929b11b2449b85e
SaraBarrett <sara.c.mon@gmail.com>
2024-10-29 15:53:28 +0000

    changes to filenames

--- src/exs/components/login.jsx
+++ src/exs/components/login.jsx
@@ -1,8 +1,27 @@
-export default function App() {
+export const user = {
+    name: '',
+  };
+  
+  function App() {
+    // Your goal: This function should be called WITH A VALUE for name when the <button> is clicked
+    function handleCreateUser(name) {
+      user.name = name;
+    }
+  
     return (
-        <div>
-            <p data-testid="price">$100</p>
-            <button>Apply Discount</button>
-        </div>
+      <div id="app">
+        <h1>User Login</h1>
+        <p>
+          <label>Name</label>
+          {/* You don't need to do anything with this input! You'll learn how to handle user input later */}
+          <input type="text" />
+        </p>
+  
+        <p id="actions">
+          <button>Create User</button>
+        </p>
+      </div>
     );
-}
+  }
+  
+  export default App;
