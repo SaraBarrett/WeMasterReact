5ae5e842267d577674d3dc3f415ad27fe1ca2793
SaraBarrett <sara.c.mon@gmail.com>
2024-11-15 10:14:52 +0000

    places

--- src/pages/AvailablePlaces.jsx
+++ src/pages/AvailablePlaces.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback  } from 'react';
 import Places from '../components/Places'
-
+import globeImg from '../assets/globe.png';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const[availablePlaces, setAvailablePlaces] = useState([]); 
+  const[availablePlaces, setAvailablePlaces] = useState([]);
+  const [userPlaces, setUserPlaces] = useState([]); 
+  
 
   useEffect(() =>
     {
@@ -16,12 +18,64 @@ export default function AvailablePlaces({ onSelectPlace }) {
     },
     []);
 
+
+    function handleStartRemovePlace(place) {
+      setModalIsOpen(true);
+      selectedPlace.current = place;
+    }
+  
+    function handleStopRemovePlace() {
+      setModalIsOpen(false);
+    }
+  
+    function handleSelectPlace(selectedPlace) {
+      setUserPlaces((prevPickedPlaces) => {
+        if (!prevPickedPlaces) {
+          prevPickedPlaces = [];
+        }
+        if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+          return prevPickedPlaces;
+        }
+        return [selectedPlace, ...prevPickedPlaces];
+      });
+    }
+  
+    const handleRemovePlace = useCallback(async function handleRemovePlace() {
+      setUserPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+      );
+  
+      setModalIsOpen(false);
+    }, []);
+  
   return (
-    <Places
-      title="Available Places"
-      places={availablePlaces}
+    <>
+    <header>
+      <img src={globeImg} alt="Stylized globe" />
+      <h1>PlacePicker</h1>
+      <p>
+        Create your personal collection of places you would like to visit or
+        you have visited.
+      </p>
+    </header>
+    <main>
+      <Places
+          title="My Selected Places ..."
       fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
+          places={userPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+
+
+      <Places
+        title="I'd like to visit ..."
+        fallbackText="Select the places you would like to visit below."
+        places={availablePlaces}
+        onSelectPlace={handleSelectPlace}
+      />
+
+
+    </main>
+  </>
   );
 }
