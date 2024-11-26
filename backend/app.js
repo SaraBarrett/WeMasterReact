9cd0fadc3780c9672bc372632a4531da703bb458
SaraBarrett <sara.c.mon@gmail.com>
2024-11-26 08:55:56 +0000

    auth

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,19 @@
 import fs from "node:fs/promises";
-
 import bodyParser from "body-parser";
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
 app.use(express.static("./images"));
 app.use(bodyParser.json());
 
-// CORS
+const SECRET_KEY = "your_secret_key";
+
+// Generate a JWT token
+const createJSONToken = (email) => {
+  return jwt.sign({ email }, SECRET_KEY, { expiresIn: "1h" });
+};
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
@@ -26,9 +31,7 @@ app.get("/places", async (req, res) => {
 
 app.get("/user-places", async (req, res) => {
   const fileContent = await fs.readFile("./data/user-places.json");
-
   const places = JSON.parse(fileContent);
-
   res.status(200).json({ places });
 });
 
@@ -38,9 +41,8 @@ app.put("/user-places", async (req, res) => {
   res.status(200).json({ message: "User places updated!" });
 });
 
+//rotas de users
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
-
   const fileContent = await fs.readFile("./data/users.json");
   const users = JSON.parse(fileContent);
 
@@ -51,7 +53,32 @@ app.post("/signup", async (req, res) => {
   res.status(200).json({ message: "User Inserted!" });
 });
 
-//rotas de users
+app.post("/login", async (req, res) => {
+  const fileContent = await fs.readFile("./data/users.json");
+  const users = JSON.parse(fileContent);
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const login = users.find((u) => u.email === email && u.password === password);
+
+  if (!login) {
+    return res.status(422).json({
+      message: "Invalid credentials.",
+      errors: { credentials: "Invalid email or password entered." },
+    });
+  }
+
+  const token = createJSONToken(email);
+
+  const AuthUser = {
+    token: token,
+    name: login.name,
+    role: login.role,
+  };
+
+  res.json(AuthUser);
+});
 
 // 404
 app.use((req, res, next) => {
